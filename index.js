aa3668970d3371024ed58972306f10576f3e40ae
Hank Liu <hanke.liu@gmail.com>
2020-10-01 12:52:07 -0400

    correct syntax

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ mongoose.connect("mongodb://localhost:27017/auth", { useNewUrlParser: true })
 // app
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
-app.user(cors());
+app.use(cors());
 router(app);
 
 // server
