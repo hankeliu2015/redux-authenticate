42783af70df85aa78ea82a7156dabb5c40bcdb96
Hank Liu <hanke.liu@gmail.com>
2020-10-01 12:44:50 -0400

    add cors and allow cors request

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 // DB
 // mongoose.connect('mongodb://localhost:auth/auth');
 mongoose.connect("mongodb://localhost:27017/auth", { useNewUrlParser: true })
@@ -12,6 +13,7 @@ mongoose.connect("mongodb://localhost:27017/auth", { useNewUrlParser: true })
 // app
 app.use(morgan('combined'));
 app.use(bodyParser.json({type: '*/*'}));
+app.user(cors());
 router(app);
 
 // server
